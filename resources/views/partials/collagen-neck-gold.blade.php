f62b7c6d0e6454cb7533d4113bcb3248a72e51f8
lexxed <lexx.tham@gmail.com>
2017-10-09 05:54:29 +0000

    add neck img & text

--- resources/views/partials/collagen-neck-gold.blade.php
+++ resources/views/partials/collagen-neck-gold.blade.php
@@ -1,1 +1,51 @@
+<br>
+<b>Which skin type is it good for?</b><br>
+✔ Normal<br>
+✔ Combination<br>
+✔ Dry<br>
+✔ Sensitive <br>
+<br>
+<b>What is it good at:</b><br>
+- Anti Wrinkles<br>
+- Moisturize <br>
+- Brighten and improve skin's texture
+<br><br>
+<b>Solutions for:</b><br>
+- Wrinkles<br>
+- Dull skin<br>
+<br>
+<b>If you want to know more…</b><br>
+An innovative sheet-form transparent neck mask composed mainly of ingredients including pure natural extracts and collagen.
+The one-way absorption rate of this product is higher than 98%, which means that the amount of nutrients and moisture it offers is 10 times more than of ordinary neck masks, in only 10-20 minutes, the ingredients condensed on this crystal mask will be gradually dissolved under body temperature and be absorbed directly by skin cells thoroughly expanded pores, providing supplementary elements such as collagen, whitening essence.
+ 
+<br><br>
+<b>What else you need to know:</b><br>
+Moisturizing factor and different kinds of vitamins, such ingredients are effective in instant an immediate soft and moisturized appearance with elasticity –four functions at the same time, if can even relieve and moderate sunburns caused by the sunbathing and laser treatment. Complete and intensive neckcare in a single mask!
+<br>
+Best silk collagen crystal neck mask are specially pre-moistened with phyto-collagen, pore moisturizing & moisturizing & lost collagen replenish exclusively by bio-fermentation technology, which has been proved to its power of collagen ravishment, deep moisturize and wrinkle smoothing.
+<br><br>
+Crystal Collagen Gold Eye Mask have been best selling mask for taiwan and highly recommended on 女人我最大 taiwan tv show on beauty.
+<br><br>
+<b>Ingredients:</b>
+<br>
+Water (Aqua), Grape Seed oil (Vitis Vinfera), Palmitate (Vatamin A), Tocophbryl Acetate (Vitamin B), Aloe Vera, Green Tea Extract & Ect.
+
+
+
+  
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
 
--- resources/views/partials/collagen-neck-white.blade.php
+++ resources/views/partials/collagen-neck-white.blade.php
@@ -1,1 +1,79 @@
+<br>
+<b>Which skin type is it good for?</b><br>
+✔ Normal<br>
+✔ Combination<br>
+✔ Dry<br>
+✔ Sensitive <br>
+<br>
+<b>What is it good at:</b><br>
+- Remove freckles<br>
+- Whitening
+<br><br>
+<b>Solutions for:</b><br>
+- Freckles<br>
+- Dull grey skin<br>
+- Pigments<br>
+<br>
+<b>If you want to know more…</b><br>
+The mask contains rich pearl powder that can remove freckles and lighten pigments.
+An innovative sheet-form transparent neck mask composed mainly of ingredients including pure natural extracts and collagen.
+The one-way absorption rate of this product is higher than 98%, which means that the amount of nutrients and moisture it offers is 10 times more than of ordinary neck masks, in only 10-20 minutes, the ingredients condensed on this crystal mask will be gradually dissolved under body temperature and be absorbed directly by skin cells thoroughly expanded pores, providing supplementary elements such as collagen, whitening essence.
+ 
+<br><br>
+<b>What else you need to know:</b><br>
+Moisturizing factor and different kinds of vitamins, such ingredients are effective in instant an immediate soft and moisturized appearance with elasticity –four functions at the same time, if can even relieve and moderate sunburns caused by the sunbathing and laser treatment. Complete and intensive neckcare in a single mask!
+<br>
+Best silk collagen crystal neck mask are specially pre-moistened with phyto-collagen, pore moisturizing & moisturizing & lost collagen replenish exclusively by bio-fermentation technology, which has been proved to its power of collagen ravishment, deep moisturize and wrinkle smoothing.
+<br><br>
+Crystal Collagen Gold Eye Mask have been best selling mask for taiwan and highly recommended on 女人我最大 taiwan tv show on beauty.
+<br><br>
+<b>Ingredients:</b>
+<br>
+Water (Aqua), Grape Seed oil (Vitis Vinfera), Palmitate (Vatamin A), Tocophbryl Acetate (Vitamin B), Aloe Vera, Green Tea Extract & Ect.
+<br><br>
+<b>How to use:</b>
+<br>
+1. Clean and thoroughly dry your neck.
+<br>
+2. After opening the foil package and peel off the plastic liner,for the best result,use the facial mask immediately after opening the packaging.
+<br>
+3. Apply the neck mask as shown,smoonth the mask gently to ensure good contact with the skin.
+<br>
+4. Wear the neck mask for about 30 minutes.
+<br>
+5. Remove the neck mask gently,donot reuse the neck mask.
+
+
+
+  
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+ 
+
 
