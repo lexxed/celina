5fb7607841df1537b5755216f50cc9057b3772f9
lexxed <lexx.tham@gmail.com>
2017-10-26 04:44:39 +0000

    add snp pearl mask

---
+++ resources/views/partials/snp-black-pearl-facial-mask.php
@@ -0,0 +1,24 @@
+<br>
+<b>Which skin type is it good for?</b><br>
+✔ Normal<br>
+✔ Combination<br>
+✔ Dry<br>
+<br>
+<b>What is it good at:</b><br>
+- Moisturizing<br>
+- Whitening<br>
+- Tighten Pores<br>
+<br>
+<b>Solutions for:</b><br>
+- Dull Skin<br>
+- Wrinkles<br>
+<br>
+<b>If you want to know more…</b><br>
+SNP Black Pearl Renew Ampoule Mask soothes, softens, and brightens skin, making it as smooth and glowy as a black pearl.
+Energizing mask contains black pearl extract which improves skin resilience and brightens skin.
+<br><br>
+<b>Ingredients: </b><br>
+Water, Glycerin, Niacinamide, Betaine, Alcohol, Pearl Extract(250mg), Soja (Soybean) Seed Extract, Sesamum Indicum (Sesame) Seed Extract, Oryza Sativa (Rice) Extract, Salvia Hispanica Seed Extract, Rubus Fruitcosus (Blackberry) Fruit Extract, Angustifolium (Blueberry) Fruit Extract, Ribes Nigrum (Black Currant) Leaf Extract, Euterpe Oleracea Fruit Extract, Aloe Barbadensis Leaf Extract, Ceramide 3, Sodium hyaluronate, Centella Asiatica Extract, Hamamelis Virginiana (Witch hazel) Extract, Camellia Leaf Extract, Chamomilla Recutita (Matricaria) Extract, Polysorbate 80, Carbomer, Hydroxyethylcellulose, Pheonxyethanol, Triethanolamine, Disodium EDTA, Perfume
+
+
+
