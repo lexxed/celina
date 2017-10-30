82145ef5064e7898628c355722d31a65bbe8b85b
lexxed <lexx.tham@gmail.com>
2017-10-30 07:14:54 +0000

    update eye & breast img

--- resources/views/partials/collagen-chest-gold.blade.php
+++ resources/views/partials/collagen-chest-gold.blade.php
@@ -1,1 +1,19 @@
+<br>
+<b>Which skin type is it good for?</b><br>
+✔ Normal<br>
+✔ Combination<br>
+✔ Dry<br>
+<br>
+<b>What is it good at:</b><br>
+- tightening and lifting of the breasts<br>
+<br>
+<b>Solutions for:</b><br>
+- Saggy Breasts<br>
+<br>
+<b>If you want to know more…</b><br>
+An innovative sheet-form transparent breast mask composed mainly of ingredients such as pure natural extracts and collagen.
+Luxurious treatment for enhancing the size and lifting the skin of your breasts.
+Effectively reversing the natural aging process and the resultant sagging breasts
+
+
 
